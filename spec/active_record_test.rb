cd44eab72000ad339e7a40e4dc5be0bffd8a8326
Vicki Hamm <vhamm@covermymeds.com>
2020-05-20 16:48:39 -0400

    Rebuilding ActiveRecord, part I

---
+++ spec/active_record_test.rb
@@ -0,0 +1,37 @@
+require "spec_helper"
+require "active_record"
+require "muffin_blog/app/models/application_record"
+require "muffin_blog/app/models/post"
+
+RSpec.describe "Post", type: :model do
+  before(:each) do
+    @post = Post.new(id: 1, title: "My first post")
+  end
+
+  it 'should have_one title' do
+    expect(@post.title).to eq("My first post")
+  end
+
+  it 'should have_one id' do
+    expect(@post.id).to eq(1)
+  end
+
+  it 'should be able to find instances in db' do
+    Post.establish_connection(
+      database: "#{__dir__}/muffin_blog/db/development.sqlite3")
+    results = Post.connection.execute("SELECT * FROM posts")
+    @post2 = Post.find(1)
+    expect(@post2).to_not be_nil
+    expect(@post2.id).to eq(1)
+    expect(@post2.title).to eq("Blueberry Muffins")
+  end
+
+  it 'should have a db connection' do
+    Post.establish_connection(
+      database: "#{__dir__}/muffin_blog/db/development.sqlite3")
+    results = Post.connection.execute("SELECT * FROM posts")
+    expect(results).to be_an_instance_of(Array)
+    row = results.first
+    expect(row).to have_key(:title)
+  end
+end
---
+++ spec/muffin_blog/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Post.create title: "Blueberry Muffins", body: "these muffins are extra large and yummy"
+
+Post.create title: "Chocolate Chip Muffins", body: "you won't believe how yummy these are"
